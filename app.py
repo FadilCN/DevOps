b5e2c661239946ce4165cd3fbba3047a6ce4cb45
FadilCN <mohammedfadilcn@gmail.com>
2025-12-09 16:27:13 +0530

    made backend

--- app.py
+++ app.py
@@ -1,14 +1,29 @@
-from flask import Flask
+from flask import Flask, request, render_template, redirect
+from pymongo.mongo_client import MongoClient
+from dotenv import load_dotenv
+import os
+
+username = os.getenv("MONGO_USER")
+password = os.getenv("MONGO_PASS")
+
+uri = f"mongodb+srv://{username}:{password}@cluster0.szp99ew.mongodb.net/?appName=Cluster0"
+client = MongoClient(uri)
+db = client["data"]
+collection = db["users"]
 
 app = Flask(__name__)
 
-@app.route("/api")
-def read_file():
-    file = open("text.txt", "r")
-    content = file.read()
-    file.close()
-    return {"text": content}  
+@app.route("/", methods=["GET"] )
+def form():
+      return render_template("form.html")
 
+@app.route("submittodoitem", methods=["POST"])
+def submit():
+     item_name=request.form.get("name")
+     item_desc=request.form.get("desc")
+     collection.insert_one({"name": item_name, 
+                            "desc": item_desc})
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
